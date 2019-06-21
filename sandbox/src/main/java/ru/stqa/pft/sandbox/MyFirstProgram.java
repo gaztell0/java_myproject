4e387f3a0ff0ba1470005459fe42e281c76b8513
Taras Shcherbak <scherbakbox@yandex.ru>
2019-06-21 16:15:18 +0300

    Обновление кода программы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,12 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    String somebody = "World";
+    System.out.println("Hello, " + somebody + "!");
+
+    double a = 8.0;
+    double s = a * a;
+    System.out.println("Площадь квадрата со стороной " + a + " = " + s);
   }
 
 }
