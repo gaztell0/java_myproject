3d80e433a30a6b28144b0e3e2e70663ccefb35c5
Taras Shcherbak (Home) <scherbakbox@yandex.ru>
2019-06-22 17:42:56 +0300

    Update Distance.java

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -6,9 +6,12 @@ public class Distance {
 
     Point p1 = new Point(-100, 0, 100, 0);
     Point p2 = new Point(100, 100, -100, -100);
+    Point p3 = new Point(-555, 1000, 765, -40);
+
 
     System.out.println("Расстояние между двумя точками на двумерной плоскости = " + p1.distance());
     System.out.println("Расстояние между двумя точками на двумерной плоскости = " + p2.distance());
+    System.out.println("Расстояние между двумя точками на двумерной плоскости = " + p3.distance());
 
   }
 
