fb32237478a077d9428879be3e33784a05726eea
Taras Shcherbak (Home) <scherbakbox@yandex.ru>
2019-06-22 17:32:29 +0300

    Вычисления реализованны с помощью метода в классе Point

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+public class Distance {
+
+  public static void main(String[] args) {
+
+    Point p1 = new Point(-100, 0, 100, 0);
+    Point p2 = new Point(100, 100, -100, -100);
+
+    System.out.println("Расстояние между двумя точками на двумерной плоскости = " + p1.distance());
+    System.out.println("Расстояние между двумя точками на двумерной плоскости = " + p2.distance());
+
+  }
+
+}
--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -13,18 +13,10 @@ public class MyFirstProgram {
     Rectangle r = new Rectangle(4, 6);
     System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-    Point p1 = new Point(10, 0);
-    Point p2 = new Point(0, 0);
-
-    System.out.println("Расстояние между двумя точками на двумерной плоскости = " + distance(p1, p2));
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
-  }
-
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
   }
 
 }
