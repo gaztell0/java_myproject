dde3fd1ca6de27383f43de71c5438ee823c73295
Taras Shcherbak <scherbakbox@yandex.ru>
2019-06-24 16:35:54 +0300

    Переделана реализация метода в классе Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -4,15 +4,10 @@ public class Distance {
 
   public static void main(String[] args) {
 
-    Point p1 = new Point(-100, 0, 100, 0);
-    Point p2 = new Point(100, 100, -100, -100);
-    Point p3 = new Point(-555, 1000, 765, -40);
+    Point p1 = new Point(100,100);
+    Point p2 = new Point(10,10);
 
-
-    System.out.println("Расстояние между двумя точками на двумерной плоскости = " + p1.distance());
-    System.out.println("Расстояние между двумя точками на двумерной плоскости = " + p2.distance());
-    System.out.println("Расстояние между двумя точками на двумерной плоскости = " + p3.distance());
-
+    System.out.println("Расстояние между двумя точками на двумерной плоскости = " + p1.distance(p2));
   }
 
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -4,19 +4,14 @@ public class Point {
 
   public double x1;
   public double y1;
-  public double x2;
-  public double y2;
 
-
-  public Point(double x1, double y1, double x2, double y2) {
+  public Point(double x1, double y1) {
     this.x1 = x1;
     this.y1 = y1;
-    this.x2 = x2;
-    this.y2 = y2;
   }
 
-  public double distance() {
-    return Math.sqrt(Math.pow((this.x2 - this.x1), 2) + Math.pow((this.y2 - this.y1), 2));
+    public double distance(Point p2) {
+      return Math.sqrt(Math.pow((p2.x1 - this.x1), 2) + Math.pow((p2.y1 - this.y1), 2));
   }
 
 }
