610b2ffce406f0cfcf2d3b5f71adfd2e0a396a20
Taras Shcherbak <scherbakbox@yandex.ru>
2019-06-24 16:45:13 +0300

    Подправлен класс PointTests и добавлены примеры вычислений расстояния между точками

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -6,8 +6,16 @@ public class Distance {
 
     Point p1 = new Point(100,100);
     Point p2 = new Point(10,10);
-
     System.out.println("Расстояние между двумя точками на двумерной плоскости = " + p1.distance(p2));
+
+    Point p3 = new Point(50,0);
+    Point p4 = new Point(10,0);
+    System.out.println("Расстояние между двумя точками на двумерной плоскости = " + p3.distance(p4));
+
+    Point p5 = new Point(0,0);
+    Point p6 = new Point(0,1000);
+    System.out.println("Расстояние между двумя точками на двумерной плоскости = " + p5.distance(p6));
+
   }
 
 }
--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -8,24 +8,27 @@ public class PointTests {
   @Test
   public void testPoint1() {
 
-    Point p1 = new Point(-50.5, 34.7, 5, 10.5);
-    Assert.assertEquals(p1.distance(),60.54659362837847);
+    Point p1 = new Point(0,10);
+    Point p2 = new Point( 0, 40);
+    Assert.assertEquals(p1.distance(p2),30.0);
 
   }
 
   @Test
   public void testPoint2() {
 
-    Point p2 = new Point(5, 5, 6, 5);
-    Assert.assertEquals(p2.distance(), 1.0);
+    Point p1 = new Point(0,0);
+    Point p2 = new Point( 0, 1000);
+    Assert.assertEquals(p1.distance(p2),1000.0);
 
   }
 
   @Test
   public void testPoint3() {
 
-    Point p3 = new Point(40, 50, 10, 50);
-    Assert.assertEquals(p3.distance(), 30.0);
+    Point p1 = new Point(0,10);
+    Point p2 = new Point( 0, 2000);
+    Assert.assertEquals(p1.distance(p2),1990.0);
 
   }
 
