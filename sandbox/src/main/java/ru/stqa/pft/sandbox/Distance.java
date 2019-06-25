0da0d95b2ee5ba6c0347ac331ed65d8fa14d38d5
Taras Shcherbak <scherbakbox@yandex.ru>
2019-06-25 16:39:06 +0300

    Смена версии jdk с 12 на 8

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -6,15 +6,16 @@ public class Distance {
 
     Point p1 = new Point(100,100);
     Point p2 = new Point(10,10);
-    System.out.println("Расстояние между двумя точками на двумерной плоскости = " + p1.distance(p2));
-
     Point p3 = new Point(50,0);
     Point p4 = new Point(10,0);
-    System.out.println("Расстояние между двумя точками на двумерной плоскости = " + p3.distance(p4));
-
     Point p5 = new Point(0,0);
     Point p6 = new Point(0,1000);
-    System.out.println("Расстояние между двумя точками на двумерной плоскости = " + p5.distance(p6));
+
+    System.out.println("Расстояние между двумя точками p1 и p2 на двумерной плоскости = " + p1.distance(p2));
+    System.out.println("Расстояние между двумя точками p3 и p4 на двумерной плоскости = " + p3.distance(p4));
+    System.out.println("Расстояние между двумя точками p5 и p6 на двумерной плоскости = " + p5.distance(p6));
+    System.out.println("Расстояние между двумя точками p1 и p6 на двумерной плоскости = " + p1.distance(p6));
+
 
   }
 
