4563a7bdc430b207828ff4d1730e346353f58597
Taras Shcherbak <scherbakbox@yandex.ru>
2019-07-11 16:15:13 +0300

    Реализована проверка и обеспечение предусловий

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -8,7 +8,13 @@ public class ContactCreationTests extends TestBase {
   @Test
   public void testContactCreation() throws Exception {
     app.getNavigationHelper().gotoAddnewPage();
-    app.getContactHelper().fillContactForm(new ContactData("Михаил", "Петрович", "Жуков", "Bug", "ADT Inc.", "Россия, г. Москва, ул. Аргуновская 35", "84957757575", "89262881045", "+74951231212", "ivanmercury@yandex.ru", "ivanmercury@gmail.com", "1989", "test1"), true);
+    app.getContactHelper().fillContactForm(new ContactData("Михаил", "Петрович",
+                                                           "Жуков", "Bug",
+                                                           "ADT Inc.", "Россия, г. Москва, ул. Аргуновская 35",
+                                                           "84957757575", "89262881045",
+                                                           "+74951231212", "ivanmercury@yandex.ru",
+                                                           "ivanmercury@gmail.com", "1989",
+                                                           "test1"), true);
     app.getContactHelper().initContactCreation();
     app.getNavigationHelper().gotoHomePage();
     app.getSessionHelper().logout();
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModification.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModification.java
@@ -9,7 +9,13 @@ public class ContactModification extends TestBase {
   public void testContactModification() {
     app.getNavigationHelper().gotoHomePage();
     app.getContactHelper().initContactModification();
-    app.getContactHelper().fillContactForm(new ContactData("Петр", "Алексеевич", "Кутузов", "Bug", "ADT Inc.", "Россия, г. Москва, ул. Аргуновская 35", "84957757575", "89262881045", "+74951231212", "ivanmercury@yandex.ru", "ivanmercury@gmail.com", "1989", null), false);
+    app.getContactHelper().fillContactForm(new ContactData("Петр", "Алексеевич",
+                                                           "Кутузов", "Bug",
+                                                           "ADT Inc.", "Россия, г. Москва, ул. Аргуновская 35",
+                                                           "84957757575", "89262881045",
+                                                           "+74951231212", "ivanmercury@yandex.ru",
+                                                           "ivanmercury@gmail.com", "1989",
+                                                           null), false);
     app.getContactHelper().submitContactModification();
     app.getNavigationHelper().gotoHomePage();
     app.getSessionHelper().logout();
