08e796bbf126a6638e8f6721945fb1a4479e6209
Taras Shcherbak <scherbakbox@yandex.ru>
2019-07-16 15:01:40 +0300

    в Класс ContactDeletion Tests добавлено сравнение списков целиком

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -23,5 +23,8 @@ public class ContactDeletionTests extends TestBase {
     Assert.assertEquals(after.size(), before.size() - 1);
     app.getSessionHelper().logout();
 
+    before.remove(before.size() - 1);
+    Assert.assertEquals(before, after);
   }
+
 }
