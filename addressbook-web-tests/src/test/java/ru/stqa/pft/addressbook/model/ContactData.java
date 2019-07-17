5adc75f1068690955b74358597d8085230e025e2
Taras Shcherbak <scherbakbox@yandex.ru>
2019-07-17 16:12:32 +0300

    В тестах для создания и модификации добавлена сортировка перед сравнением

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -19,7 +19,7 @@ public class ContactData {
   }
 
   public ContactData(String firstname, String lastname, String group) {
-    this.id = 0;
+    this.id = Integer.MAX_VALUE;
     this.firstname = firstname;
     this.lastname = lastname;
     this.group = group;
@@ -59,14 +59,14 @@ public class ContactData {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     ContactData that = (ContactData) o;
-    return id == that.id &&
-            Objects.equals(firstname, that.firstname) &&
+    return Objects.equals(firstname, that.firstname) &&
             Objects.equals(lastname, that.lastname);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(id, firstname, lastname);
+    return Objects.hash(firstname, lastname);
   }
+
 
 }
