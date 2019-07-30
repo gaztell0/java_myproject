e7f21bd416fedfbd23c6b63edcaae22449dc8984
Taras Shcherbak <scherbakbox@yandex.ru>
2019-07-30 15:00:25 +0300

    добавлен анализ опций командой строки при запуске генератора данных контактов
    Библиотека JCommander

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 import java.io.File;
@@ -11,16 +14,30 @@ import java.util.List;
 
 public class ContactDataGenerator {
 
+  @Parameter(names = "-c", description = "Contact count")
+  public int count;
+
+  @Parameter (names = "-f", description = "Target file")
+  public String file;
+
   public static void main(String[] args) throws IOException {
-    int count = Integer.parseInt(args[0]);
-    File file = new File(args[1]);
-
-    List<ContactData> contacts = generateContacts(count);
-    save(contacts, file);
-
+    ContactDataGenerator generator = new ContactDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch (ParameterException ex) {
+      jCommander.usage();
+      return;
+    }
+    generator.run();
   }
 
-  private static void save(List<ContactData> contacts, File file) throws IOException {
+  private void run() throws IOException {
+    List<ContactData> contacts = generateContacts(count);
+    save(contacts, new File(file));
+  }
+
+  private void save(List<ContactData> contacts, File file) throws IOException {
     Writer writer = new FileWriter(file);
     for (ContactData contact: contacts){
       writer.write(String.format("%s;%s;%s\n", contact.getFirstname(), contact.getLastname(),contact.getHomeAddress()));
@@ -28,11 +45,11 @@ public class ContactDataGenerator {
     writer.close();
   }
 
-  private static List<ContactData> generateContacts(int count) {
+  private List<ContactData> generateContacts(int count) {
     List<ContactData> contacts = new ArrayList<ContactData>();
     for (int i = 0; i < count ; i++) {
-      contacts.add(new ContactData().withFirstname(String.format("test%s", i))
-      .withLastname(String.format("test%s", i)).withHomeAddress(String.format("test%s", i)));
+      contacts.add(new ContactData().withFirstname(String.format("test %s", i))
+      .withLastname(String.format("test %s", i)).withHomeAddress(String.format("test %s", i)));
     }
     return contacts;
   }
