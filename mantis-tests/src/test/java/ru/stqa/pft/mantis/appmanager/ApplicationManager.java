326b13b59b155eeeb7dddc953c56858da1ece518
Taras Shcherbak <scherbakbox@yandex.ru>
2019-08-19 18:23:33 +0300

    Промежуточная точка урок 8.6

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -20,6 +20,7 @@ public class ApplicationManager {
   private String browser;
   private RegistrationHelper registrationHelper;
   private FtpHelper ftp;
+  private MailHelper mailHelper;
 
 
   public ApplicationManager(String browser) {
@@ -76,6 +77,11 @@ public class ApplicationManager {
     return wd;
   }
 
-
+  public MailHelper mail() {
+    if (mailHelper == null) {
+      mailHelper = new MailHelper(this);
+    }
+    return mailHelper;
+  }
 
 }
